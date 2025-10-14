9753eb6ff4de759b300084963020ad159177a32b
Rishi <rishi@Rishis-MacBook-Air.local>
2025-10-15 02:56:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Welcome to my react project</h1>
+        <h1>CI/CD devops lab</h1>
+        <p>actions deployment</p>
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
